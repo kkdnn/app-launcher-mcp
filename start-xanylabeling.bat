1d3892bc4d9092d22114062efb213fe543e4fff1
kkdnn <139769735+kkdnn@users.noreply.github.com>
2026-08-13 18:08:25 +0800

    Add start-xanylabeling.bat

---
+++ start-xanylabeling.bat
@@ -0,0 +1,4 @@
+@echo off
+rem X-AnyLabeling 启动器：激活 conda 环境 kkk 后启动 GUI
+call C:\Users\12579\miniconda3\Scripts\activate.bat kkk
+start "" "C:\Users\12579\miniconda3\envs\kkk\Scripts\xanylabeling.exe"
